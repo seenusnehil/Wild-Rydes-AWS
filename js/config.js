a84721e146146d35b218d57599c3c476064eb2ae
Snehil Seenu <snehilseenu@gmail.com>
2023-04-03 00:04:03 +0530

    new project added

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_4kBIrcM8K', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'd564vgusi1ks06i88ff2iuu4j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://0igmah3zsd.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
